66f3bd4c6e94bc32a98f56d4d860bb8a5344578c
dipesh <kcdipesh429@gmail.com>
2023-02-06 20:33:05 -0600

    feat: add role domain

---
+++ src/main/java/edu/miu/waa/project/backend/domain/Role.java
@@ -0,0 +1,14 @@
+package edu.miu.waa.project.backend.domain;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String role;
+}
+
