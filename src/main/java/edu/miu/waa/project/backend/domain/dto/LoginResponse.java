58ef5e27af1beab69cf21158a6e451b29b5d2ea4
dipesh <kcdipesh429@gmail.com>
2023-02-06 20:33:29 -0600

    feat: add dtos for user and authentication

---
+++ src/main/java/edu/miu/waa/project/backend/domain/dto/LoginResponse.java
@@ -0,0 +1,11 @@
+package edu.miu.waa.project.backend.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+    private String accessToken;
+    private String refreshToken;
+}
