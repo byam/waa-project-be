863e6c2a99e6bce5b0956e21a8422b18648bb0c7
dipesh <kcdipesh429@gmail.com>
2023-02-07 15:59:12 -0600

    feat: add repository for role

--- src/main/java/edu/miu/waa/project/backend/repo/RoleRepo.java
+++ src/main/java/edu/miu/waa/project/backend/repo/RoleRepo.java
@@ -1,14 +1,12 @@
 package edu.miu.waa.project.backend.repo;
 
 import edu.miu.waa.project.backend.domain.Role;
-import edu.miu.waa.project.backend.enumSet.RoleValue;
+import edu.miu.waa.project.backend.enumSet.RoleType;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
 
 public interface RoleRepo extends CrudRepository<Role,Long> {
 
 //    Role findByRole(String role);
 
-    Role findByRole(RoleValue role);
+    Role findByRole(RoleType role);
 }
