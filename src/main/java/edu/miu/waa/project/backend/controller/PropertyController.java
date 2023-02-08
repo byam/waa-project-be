ecbb13efcbacd85181e67c96e953edba58f89501
dipesh <kcdipesh429@gmail.com>
2023-02-08 11:22:01 -0600

    hotfix: add city,state,owner for filter request

--- src/main/java/edu/miu/waa/project/backend/controller/PropertyController.java
+++ src/main/java/edu/miu/waa/project/backend/controller/PropertyController.java
@@ -4,9 +4,9 @@ import edu.miu.waa.project.backend.domain.dto.PropertyDto;
 import edu.miu.waa.project.backend.domain.dto.request.PropertyFilterRequest;
 import edu.miu.waa.project.backend.domain.dto.response.HttpResponse;
 import edu.miu.waa.project.backend.enumSet.ListingType;
+import edu.miu.waa.project.backend.enumSet.PropertyStatus;
 import edu.miu.waa.project.backend.enumSet.PropertyType;
 import edu.miu.waa.project.backend.service.PropertyService;
-import edu.miu.waa.project.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +19,7 @@ import java.util.List;
 public class PropertyController {
 
     private final PropertyService propertyService;
-    private final UserService userService;
+
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -28,10 +28,17 @@ public class PropertyController {
             @RequestParam(name = "min_price", required = false) Double minPrice,
             @RequestParam(name = "max_price", required = false) Double maxPrice,
             @RequestParam(name = "listing_type", required = false) ListingType listingType,
-            @RequestParam(name = "property_type", required = false) PropertyType propertyType
+            @RequestParam(name = "property_type", required = false) PropertyType propertyType,
+            @RequestParam(name = "owner_id", required = false) Long ownerId,
+            @RequestParam(name = "property_status", required = false) PropertyStatus propertyStatus,
+            @RequestParam(name = "city", required = false) String city,
+            @RequestParam(name = "state", required = false) String state
 
     ) {
-        PropertyFilterRequest filters = PropertyFilterRequest.builder().minPrice(minPrice).maxPrice(maxPrice).listingType(listingType).propertyType(propertyType).build();
+        PropertyFilterRequest filters = PropertyFilterRequest.builder().minPrice(minPrice).maxPrice(maxPrice)
+                .listingType(listingType).propertyType(propertyType)
+                .city(city).state(state).propertyStatus(propertyStatus).ownerId(ownerId)
+                .build();
 
         return propertyService.findAll(filters);
     }
